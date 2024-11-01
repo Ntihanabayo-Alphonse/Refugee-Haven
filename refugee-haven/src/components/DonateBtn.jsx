fdb2ad760a95750a62253ab6b2f07155edb8bfae
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-11-01 18:10:33 +0300

    style donate button across the website

--- refugee-haven/src/components/DonateBtn.jsx
+++ refugee-haven/src/components/DonateBtn.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const DonateBtn = () => {
   return (
     <>
-        <button>🤍 Donate</button>
+        <Link to="https://donate.stripe.com/test_7sI03fdqj6DGgRGeUU" target='_blank'><button className='bg-[#F4A261] py-2 px-10 rounded-[50px] text-lg text-white font-bold'>🤍 Donate</button></Link>
     </>
   )
 }
