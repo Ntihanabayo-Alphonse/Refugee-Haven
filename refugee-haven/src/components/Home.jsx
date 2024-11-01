39bbd851498869ae1bbf7bd0455b3341268531b2
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-11-01 23:21:42 +0300

    make about us and programs section responsiveness

--- refugee-haven/src/components/Home.jsx
+++ refugee-haven/src/components/Home.jsx
@@ -16,9 +16,9 @@ const Home = () => {
             {/* Banner Section */}
             <div className="banner-section relative">
                 <div className="banner-image"></div>
-                <div className="banner-info absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <h1 className='font-bold text-white text-4xl'>A Safe Haven for New Beginnings</h1>
-                    <p className='text-[#E9E4D3] mb-6'>Empowering Refugees to Rebuild and Thrive in Supportive Communities</p>
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full sm:w-auto px-8 sm:px-0 text-center sm:text-start">
+                    <h1 className='font-bold text-white lg:text-4xl sm:text-3xl text-2xl'>A Safe Haven for New Beginnings</h1>
+                    <p className='text-[#E9E4D3] mb-6 text-'>Empowering Refugees to Rebuild and Thrive in Supportive Communities</p>
                     <div className="button">
                         <DonateBtn />
                     </div>
@@ -26,15 +26,15 @@ const Home = () => {
             </div>
 
             {/* About Us */}
-            <div className="about-us grid grid-cols-2 gap-20 px-40 py-24">
-                <div className="about-img bg-slate-500">
+            <div className="grid sm:grid-cols-2 grid-cols-1 gap-6 md:gap-10 lg:px-32 px-16 max-[400px]:px-10  py-24">
+                <div>
                     <img className='h-full' src="/images/about-bg.jpeg" alt="About Us" />
                 </div>
-                <div className="about-info">
-                    <h2 className='text-3xl text-[#1B3A57] font-bold'>About Us</h2>
-                    <p className='mb-6'>Refugee Haven is a compassionate, community-centered organization dedicated to empowering refugees and displaced individuals on their journey to rebuilding lives.</p>
-                    <h3 className='text-xl font-bold text-[#1B3A57]'>Mission</h3>
-                    <p className='mb-6 ml-4'>To provide safe, welcoming spaces where refugees can find resources, support, and a strong sense of belonging as they adjust to new communities.
+                <div>
+                    <h2 className='md:text-3xl text-2xl text-[#1B3A57] font-bold'>About Us</h2>
+                    <p className='mb-6 text-[15px] md:text-[16px]'>Refugee Haven is a compassionate, community-centered organization dedicated to empowering refugees and displaced individuals on their journey to rebuilding lives.</p>
+                    <h3 className='sm:text-xl text-lg font-bold text-[#1B3A57]'>Mission</h3>
+                    <p className='mb-6 ml-4 text-[15px] md:text-[16px]'>To provide safe, welcoming spaces where refugees can find resources, support, and a strong sense of belonging as they adjust to new communities.
                         <br />
                         <br />
                         At Refugee Haven, we understand that the path to a stable and fulfilling life begins with essential support, so we focus on providing housing, healthcare, education, and vocational training tailored to each individual's needs.</p>
