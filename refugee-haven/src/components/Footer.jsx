0787830cd560d568a2df8d67fd6bda353396b005
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-11-01 14:35:08 +0300

    styled the footer

--- refugee-haven/src/components/Footer.jsx
+++ refugee-haven/src/components/Footer.jsx
@@ -6,33 +6,34 @@ const Footer = () => {
     return (
         <>
             {/* Footer Section */}
-            <div className="footer">
-                <div className="organization-details">
-                    <div className="logo h-14 w-14 bg-[#E9E4D3] rounded-full">
+            <div className="footer flex justify-between px-28 py-16 bg-[#333333]">
+                <div className="organization-details w-[300px]">
+                    <div className="logo h-20 w-20 bg-[#E9E4D3] rounded-full">
                         <Link to="/"><img className='w-full h-full  object-cover' src={refugeeHavenLogo} alt="refugee haven logo" /></Link>
                     </div>
-                    <p>Refugee Haven is a compassionate, community-centered organization dedicated to empowering refugees and displaced individuals on their journey to rebuilding lives.</p>
+                    <p className='text-[#E9E4D3] my-8'>Refugee Haven is a compassionate, community-centered organization dedicated to empowering refugees and displaced individuals on their journey to rebuilding lives.</p>
                     <div className="social-links">
-                        <span><i className='fa fa-facebook'></i></span>
-                        <span><i className='fa fa-instagram'></i></span>
-                        <span><i className='fa fa-twitter'></i></span>
+                        <a href='/' className='border border-[#E9E4D3] rounded-full px-2 py-2 mr-4 text-lg duration-500 hover:bg-[#F4A261]'><i className='text-[#E9E4D3] fa fa-facebook'></i></a>
+                        <a href='/' className='border border-[#E9E4D3] rounded-full px-2 py-2 mr-4 text-lg duration-500 hover:bg-[#F4A261]'><i className='text-[#E9E4D3] fa fa-instagram'></i></a>
+                        <a href='/' className='border border-[#E9E4D3] rounded-full px-2 py-2 mr-4 text-lg duration-500 hover:bg-[#F4A261]'><i className='text-[#E9E4D3] fa fa-twitter'></i></a>
+                        <a href='/' className='border border-[#E9E4D3] rounded-full px-2 py-2 mr-4 text-lg duration-500 hover:bg-[#F4A261]'><i className='text-[#E9E4D3] fa fa-linkedin'></i></a>
                     </div>
                 </div>
 
                 {/* Quick links */}
                 <ul className='quick-links'>
-                    <h4>Quick Links</h4>
-                    <li><Link to="/" className='px-6 py-4 text-[#F4A261] font-bold'>Home</Link></li>
-                    <li><Link to="/programs" className='px-5 py-4 text-[#E9E4D3] font-bold hover:text-[#F4A261] duration-500'>Programs</Link></li>
-                    <li><Link to="/donate" className='px-5 py-4 text-[#E9E4D3] font-bold hover:text-[#F4A261] duration-500'>Donate</Link></li>
+                    <h4 className='font-bold text-white mb-3 text-xl'>Quick Links</h4>
+                    <li className='my-3'><Link to="/" className='py-4 text-[#F4A261] hover:underline'>Home</Link></li>
+                    <li className='my-3'><Link to="/programs" className='py-4 text-[#F4A261] hover:underline'>Programs</Link></li>
+                    <li className='my-3'><Link to="/donate" className='py-4 text-[#F4A261] hover:underline'>Donate</Link></li>
                 </ul>
 
                 {/* Contact Us */}
                 <div className="contact-us">
-                    <h4>Contact Us</h4>
-                    <p><i className='fa fa-location'></i> Kyaka II Refugee Settlement <br /> <span>Bukere, Kyegegwa</span></p>
-                    <p><i className='fa fa-phone'></i> +256 709 247 206</p>
-                    <p><i className='fa fa-mail'></i> refugeehaven@gmail.com</p>
+                    <h4 className='font-bold text-white mb-3 text-xl'>Contact Us</h4>
+                    <p className='text-[#E9E4D3] mb-4'><i className='mr-3 text-[#F4A261] fa fa-location'></i> Kyaka II Refugee Settlement <br /> <span className='ml-8 text-sm text-[#d6d6d6]'>Bukere, Kyegegwa</span></p>
+                    <p className='text-[#E9E4D3] mb-4'><i className='mr-3 text-[#F4A261] fa fa-phone'></i> +256 709 247 206</p>
+                    <p className='text-[#E9E4D3] mb-4'><i className='mr-3 text-[#F4A261] fa fa-envelope'></i> refugeehaven@gmail.com</p>
                 </div>
             </div>
         </>
