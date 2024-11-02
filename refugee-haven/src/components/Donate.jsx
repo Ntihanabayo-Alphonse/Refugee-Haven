9f05b45c8d2fae286e78646e655ef89cd46819a4
Ntihanabayo-Alphonse <engineerfipaca256@gmail.com>
2024-11-02 11:26:53 +0300

    worked on donate page responsiveness

--- refugee-haven/src/components/Donate.jsx
+++ refugee-haven/src/components/Donate.jsx
@@ -9,16 +9,16 @@ const Donate = () => {
   return (
     <>
        {location.pathname === "/donate" && (<PageBanner />)} 
-        <div className="donation-section py-16">
-            <h2 className='text-3xl text-[#1B3A57] font-bold text-center mb-14'>Join Us in Making a Difference</h2>
-            <div className="donation-container relative">
-                <div className="donation-bg h-[500px] w-full">
+        <div className="py-16">
+            <h2 className='sm:text-3xl text-2xl text-[#1B3A57] font-bold text-center mb-14'>Join Us in Making a Difference</h2>
+            <div className="relative">
+                <div className="h-[500px] w-full">
                     <img className='h-full w-full object-cover' src="/images/donate-bg.jpg" alt="donate background" />
                 </div>
-                <div className="donation-card absolute top-1/2 left-2/3 -translate-x-1/2 -translate-y-1/2  w-1/3 p-16 rounded-xl bg-[#F2F2F2]">
-                    <h1 className='text-3xl text-[#333333] font-bold mb-8'>They Need Your Help!!</h1>
-                    <p className='text-xl mb-4'>Your contribution brings safety, stability, and hope to refugee families.</p>
-                    <p className='text-xl mb-8'>Donate with kindness. Every amount donated by you counts.</p>
+                <div className="absolute top-1/2 md:left-2/3 left-1/2 -translate-x-1/2 -translate-y-1/2  xl:w-1/3 lg:w-2/5 md:w-1/2 w-[90%] p-10 xl:p-14 md:p-12 rounded-xl bg-[#F2F2F2]">
+                    <h1 className='lg:text-3xl text-2xl text-center md:text-start text-[#333333] font-bold mb-8'>They Need Your Help!!</h1>
+                    <p className='lg:text-xl text-lg md:mb-4 mb-3'>Your contribution brings safety, stability, and hope to refugee families.</p>
+                    <p className='lg:text-xl text-lg mb-8'>Donate with kindness. Every amount donated by you counts.</p>
                     <DonateBtn />
                 </div>
             </div>
